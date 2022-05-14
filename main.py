127e645772730080a6b51cec1d8643696807f2e4
alexxme211 <92181364+alexxme211@users.noreply.github.com>
2022-05-14 18:15:39 +0300

    return aggregation

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+
+results = pd.DataFrame()
+
+for file in os.listdir('stockLicenta'):
+    filename = os.fsdecode(file)
+    combo = pd.read_csv(r'stockLicenta/'+filename, usecols=["Date","Adj Close"])
+    combo.at[0, "Invested"] = 2500
+    ticker = filename.replace(".csv", '')
+    for row in range(1,627):
+        combo.at[row,"Invested"] = (combo.iloc[row]["Adj Close"] / combo.iloc[row-1]["Adj Close"]) * combo.iloc[row-1]["Invested"]
+    results.at[0,ticker] = combo.iloc[626]["Invested"]
+    results.to_csv('./aggregate.csv', index=False)
