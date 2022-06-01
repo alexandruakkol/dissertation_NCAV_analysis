ed08a40f1c29ca75274c12a87d397651f1266597
alexxme211 <92181364+alexxme211@users.noreply.github.com>
2022-06-01 16:19:40 +0300

    added gold

--- goldRegression.py
+++ goldRegression.py
@@ -9,7 +9,6 @@ inflation = pd.read_csv('data/inflation/weeklyInflation1979.csv', usecols=["Valu
 
 gold , bench, inflation = list(gold["Price"]), list(bench["Price"]), list(inflation["Value"])
 
-gold = list(map(lambda x: x.replace(',',''),gold))
 
 gold = np.array(gold).reshape(-1, 1)
 gold = gold.astype(np.float)
@@ -17,3 +16,7 @@ gold = gold.astype(np.float)
 X_stat = sm.add_constant(inflation)
 regression = sm.OLS(gold, X_stat).fit()
 print('Gold/Inflation', regression.summary())
+
+Y_stat = sm.add_constant(inflation)
+regression = sm.OLS(bench, Y_stat).fit()
+print('S&P500/Inflation', regression.summary())
